b724c451ed59d3024b61e957c6ef6cdf9214a21b
Jose Duin <joseduin@Joses-Mac.local>
2018-02-03 00:18:55 -0400

    Initial Commit

---
+++ ec charter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ec charter
+//
+//  Created by Jose Duin on 2/3/18.
+//  Copyright © 2018 Jose Duin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
